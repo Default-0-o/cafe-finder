8237711d6a5a95858e4b4f7a1784bfca8e826fd6
parhamfarnay <prhmfrny@gmail.com>
2024-03-11 13:56:55 +0330

    Revert "Revert "temporary commit to cherry pick""
    This reverts commit d9e9aa378999967c15421e6988f7a15b82005999.

---
+++ components/pages/product/index.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import ProductInfo from "./productInfo";
+import ProductDescription from "./ProductDescription";
+
+function ProductComponent() {
+  return (
+    <div>
+      <ProductInfo/>
+      <ProductDescription />
+    </div>
+  );
+}
+
+export default ProductComponent;
---
+++ pages/product/[id].jsx
@@ -0,0 +1,8 @@
+import ProductComponent from "@/components/pages/product";
+import React from "react";
+
+function ProductPage() {
+  return <ProductComponent />;
+}
+
+export default ProductPage;
