a424a4590df5acf8dc089a46b403a042926c0b0a
parhamfarnay <prhmfrny@gmail.com>
2024-03-11 13:53:25 +0330

    temporary commit to cherry pick

--- components/pages/Category/index.jsx
+++ components/pages/Category/index.jsx
@@ -2,8 +2,7 @@ import React from 'react';
 
 const CategoryComponent = () => {
     return (
-        <div>
-            
+        <div> 
         </div>
     );
 }
---
+++ components/pages/product/productInfoImages.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductInfoImages() {
+  return (
+    <div></div>
+  )
+}
+
+export default ProductInfoImages
