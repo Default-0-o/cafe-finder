93c0b46a6cb15da3c1c855de9e1076c60ada4965
parhamfarnay <prhmfrny@gmail.com>
2024-05-15 14:56:16 +0330

    parham`s last update

---
+++ pages/about/index.jsx
@@ -0,0 +1,12 @@
+import AboutComponent from "@/components/pages/about";
+import React from "react";
+
+function index() {
+  return (
+    <>
+      <AboutComponent />
+    </>
+  );
+}
+
+export default index;
